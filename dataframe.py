6e6253c12a69b255a223758790016696be4dcf5c
nicolasguasca1 <nicolas.guasca@gmail.com>
2023-08-25 21:46:21 +0200

    datafram script finds 1k rows&writes to each dir

---
+++ dataframe.py
@@ -0,0 +1,105 @@
+import os
+import pandas as pd
+import json
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--folder_to_filter', type=str,
+                    help='Folder name of the attachments to filter')
+args = parser.parse_args()
+
+content_folder = args.folder_to_filter
+parent_path = os.path.dirname(content_folder)
+
+# List to store records
+data_dict = {}
+quantity_threshold = 1000
+# directory_name = os.path.basename(content_folder)
+directory_name = os.path.abspath(content_folder)
+output_compiled_directory = f"{directory_name}_Compiled"
+# os.makedirs(output_compiled_directory, exist_ok=True)
+# EXPERIMENT________________________________________________
+# Iterate over all files in the 'Content' folder
+for root, _, files in os.walk(content_folder):
+    dir_b_path = os.path.join(content_folder, root)
+    directory_data = {}
+    filtered_dfs = []
+    for file in files:
+        filtered_rows = None
+        if file.endswith('.csv'):
+            csv_file_path = os.path.join(dir_b_path, file)
+            with open(csv_file_path, 'r') as csv_file:
+                df = pd.read_csv(csv_file)
+                # Check for lines where 'Quantity' column is 1000 or more
+
+                filtered_rows = df[df['Quantity'] >= quantity_threshold]
+                if filtered_rows is not None:
+                    filtered_dfs.append(filtered_rows)
+                    
+                file_data = []
+
+                for index, row in filtered_rows.iterrows():
+                    record = {
+                        # Assuming 'ISRC' is the column name
+                        'ISRC': row['ISRC'],
+                        'Quantity': row['Quantity']
+                    }
+                    file_data.append(record)
+                if file_data:
+                    directory_data[file] = file_data
+            output_file_compiled = "Rows_Above_1000.csv"
+            # output_file_compiled = os.path.join(
+            #     root, "_Rows_Above_1000.csv")
+            # Create the complete path for the output CSV file
+            output_csv_path = os.path.join(root, output_file_compiled)
+            # Concatenate all filtered DataFrames into a single DataFrame
+            result_df = pd.concat(filtered_dfs, ignore_index=True)
+            # Write the concatenated DataFrame to a CSV file
+            result_df.to_csv(output_csv_path, index=False)
+    if directory_data:
+        data_dict[directory_name] = directory_data
+
+
+# Save the records to a JSON file in valid JSON format
+output_file = 'filtered_cell_records.json'
+with open(output_file, 'w') as json_file:
+    json.dump(data_dict, json_file, indent=4)
+
+print(f"Files path is '{files}'")
+print(f"Filtered records saved to '{output_file}'")
+print(
+    f"Enterprise folder filtered records saved to '{output_compiled_directory}'")
+
+
+# EXPERIMENT________________________________________________
+
+# # Get a list of subdirectories inside the 'Content' folder
+# subdirectories = [subdir for subdir in os.listdir(
+#     content_folder) if os.path.isdir(os.path.join(content_folder, subdir))]
+
+# # Iterate over subdirectories
+# for subdir in subdirectories:
+#     subfolder_path = os.path.join(content_folder, subdir)
+
+#     # Get a list of CSV files in the subdirectory
+#     csv_files = [file for file in os.listdir(
+#         subfolder_path) if file.endswith('.csv')]
+
+#     if csv_files:
+#         csv_file_path = os.path.join(
+#             subfolder_path, csv_files[0])  # Get the first CSV file
+
+#         # Read the CSV file into a DataFrame
+#         df = pd.read_csv(csv_file_path)
+
+#         # Print the values of the first and second lines
+#         if len(df) >= 2:
+#             first_line = df.iloc[0]
+#             second_line = df.iloc[1]
+#             print(f"Subdirectory: {subdir}, CSV File: {csv_files[0]}")
+#             print("Values in the first line:")
+#             print(first_line)
+#             print("Values in the second line:")
+#             print(second_line)
+#             print("-" * 40)
+#         break
--- filter_per_cell.py
+++ filter_per_cell.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import pandas as pd
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -11,30 +12,79 @@ parent_directory = args.folder_to_filter
 
 directories_above_threshold = []
 
+threshold = 1000
+output_file = 'filtered_data.csv'
+# List to store filtered DataFrames from different directories
+
+
 for dir_b in os.listdir(parent_directory):
     dir_b_path = os.path.join(parent_directory, dir_b)
-
+    filtered_dfs = []
     if os.path.isdir(dir_b_path):
+        cells = {}
         for filename in os.listdir(dir_b_path):
+            filtered_df = None
             if filename.endswith('.csv'):
                 csv_path = os.path.join(dir_b_path, filename)
-
                 with open(csv_path, 'r') as csv_file:
-                    csv_reader = csv.DictReader(csv_file)
+                    # EXPERIMENTATION
+                    df = pd.read_csv(csv_file)
+                    # Find the index of the 'Score' column
+                    quantity_column = df.columns[df.columns == 'Quantity'][0]
+                    ISRC_column = df.columns[df.columns == 'ISRC'][0]
 
-                    # Check if the file is empty before trying to read the first row
-                    try:
-                        first_row = next(csv_reader)
-                    except StopIteration:
-                        continue  # Move to the next file
-
-                    if 'Quantity' in first_row and int(first_row['Quantity']) > 1000:
+                    # Filter rows where the 'Score' column is above the threshold
+                    filtered_df = df[df[quantity_column] >= threshold]
+                    # Append the filtered DataFrame to the list
+                    filtered_dfs.append(filtered_df)
+                    if filtered_df is not None:
                         directories_above_threshold.append([dir_b, csv_path])
-                        break  # No need to check other files in this directory
+
+                    # Print the filtered DataFrame
+                    # print(filtered_df)
+
+            #         csv_reader = csv.reader(csv_file)
+            #         # Check if the file is empty before trying to read the first row
+            #         try:
+            #             first_row = next(csv_reader)
+            #         except StopIteration:
+            #             continue  # Move to the next file
+            #         quantity_index = None
+            #         ISRC_index = None
+            #         for index, column in enumerate(first_row):
+            #             if column == 'Quantity':
+            #                 quantity_index = index
+            #             if column == 'ISRC':
+            #                 ISRC_index = index
+            #                 break  # Exit the loop once the columns are found
+
+            #         if quantity_index is None and ISRC_index is None:
+            #             print("Indexes not found in the CSV.")
+            #         else:
+
+            #             for line in csv_reader:
+            #                 # values = line.strip().split(',')  # Split the line into a list of values
+            #                 # if quantity_index < len(line):
+            #                 # Assuming the quantity is at index 2
+            #                 quantity = int(line[quantity_index])
+
+            #                 if quantity >= threshold:
+            #                     print(
+            #                         f"File: {csv_path}, with ISRC: {line[ISRC_index]}, has a Quantity: {quantity} which is above the threshold.")
+
+            # # if 'Quantity' in first_row and int(first_row[quantity_index]) >= 1000:
+            #                 directories_above_threshold.append(
+            #                     [dir_b, csv_path])
+                # break  # No need to check other files in this directory
+        # Concatenate all filtered DataFrames into a single DataFrame
+        result_df = pd.concat(filtered_dfs, ignore_index=True)
+        # Write the concatenated DataFrame to a CSV file
+        result_df.to_csv(output_file, index=False)
         # Step 6: Create 'directories_above.txt'
-        with open('directories_above_Quantity.txt', 'w') as dir_file:
-            for dir_name in directories_above_threshold:
-                dir_file.write(f"{dir_name}\n")
+    with open('directories_above_Quantity.txt', 'w') as dir_file:
+        for dir_name in directories_above_threshold:
+            dir_file.write(f"{dir_name}\n")
+print(f"Filtered data from all directories saved to '{output_file}'")
 
 print("Directories with CSV files where 'Quantity' is above 1000:",
       directories_above_threshold)
